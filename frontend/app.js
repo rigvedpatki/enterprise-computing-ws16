5d9bde845c0153349662ae996a9e798bcddc1819
Maxim Tschumak <maxim-tschumak@users.noreply.github.com>
2015-12-08 07:42:30 +0100

    added custom basic auth for each role

--- frontend/app.js
+++ frontend/app.js
@@ -9,7 +9,7 @@ var auth = require('basic-auth');
 
 var MANAGER_USERNAME = 'manager';
 var MANAGER_PASSWORD = 'O`M:fX3O"E[gxfT}S+/l';
-var EMPLOYEE_USERNAME = 'empployee';
+var EMPLOYEE_USERNAME = 'employee';
 var EMPLOYEE_PASSWORD = ')AybMslaS/p|o[si;xlv';
 
 app.use(express.static(__dirname + '/public'));
@@ -25,43 +25,95 @@ app.listen(appEnv.port, '0.0.0.0', function () {
 
 // EMPLOYEE main screen (add a request and list all requests)
 app.get('/employee', function (req, res) {
-    var credentials = auth(req);
-    if (!credentials || credentials.name !== EMPLOYEE_USERNAME || credentials.pass !== EMPLOYEE_PASSWORD) {
+    if (employeeAuth(req)) {
+        //TODO
+        res.render('employee', {title: 'Hey', message: 'Hello Employee!'});
+    } else {
         res.statusCode = 401;
         res.setHeader('WWW-Authenticate', 'Basic realm="example"');
         res.end('Access denied');
-    } else {
-        res.render('employee', {title: 'Hey', message: 'Hello Employee!'});
     }
 });
 
 // EMPLOYEE edit request screen (edit the values of one particular request)
 app.get('/employee/:id', function (req, res) {
-    var requestId = req.params['id'];
-    api.dummyFunction(requestId, function (err, response) {
-        res.render('employee-edit', {title: 'Hey', message: response});
-    });
+    if (employeeAuth(req)) {
+        var requestId = req.params['id'];
+        api.dummyFunction(requestId, function (err, response) {
+            res.render('employee-edit', {title: 'Hey', message: response});
+        });
+    } else {
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm="example"');
+        res.end('Access denied');
+    }
 });
 
 // MANAGER screen (list of all pending requests)
 app.get('/manager', function (req, res) {
-    res.render('manager', {title: 'Hey', message: 'Hello Manager!'});
+    if (managerAuth(req)) {
+        res.render('manager', {title: 'Hey', message: 'Hello Manager!'});
+    } else {
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm="example"');
+        res.end('Access denied');
+    }
 });
 
 // creates a new request (used by the form in the employee screen)
 app.post('/employee/requests', function (req, res) {
-    //TODO
-    // api.createRequest(..
+    if (employeeAuth(req)) {
+        //TODO
+        // api.createRequest(..
+    } else {
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm="example"');
+        res.end('Access denied');
+    }
 });
 
 // should be used to overwrite the request's values (by employee)
 app.put('/employee/requests/:id', function (req, res) {
-    //TODO
-    // api.createRequest(..
+    if (employeeAuth(req)) {
+        var requestId = req.params['id'];
+        //TODO
+        // api.updateRequest(..
+    } else {
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm="example"');
+        res.end('Access denied');
+    }
 });
 
 // should be used to overwrite the request's status (by manager)
 app.put('/manager/requests/:id/status', function (req, res) {
-    //TODO
-    // api.setStatus(..
+    if (managerAuth(req)) {
+        var requestId = req.params['id'];
+        //TODO
+        // api.setStatus(..
+    } else {
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm="example"');
+        res.end('Access denied');
+    }
 });
+
+// reads basic auth user and password from the request header
+managerAuth = function (req) {
+    var credentials = auth(req);
+    if (!credentials || credentials.name !== MANAGER_USERNAME || credentials.pass !== MANAGER_PASSWORD) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+// reads basic auth user and password from the request header
+employeeAuth = function (req) {
+    var credentials = auth(req);
+    if (!credentials || credentials.name !== EMPLOYEE_USERNAME || credentials.pass !== EMPLOYEE_PASSWORD) {
+        return false;
+    } else {
+        return true;
+    }
+};
