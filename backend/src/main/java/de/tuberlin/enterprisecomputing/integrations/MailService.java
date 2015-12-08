f51cce20b7a9b3cddff289312b8ff936cdaf56ab
The-Anh <theanhly@mailbox.tu-berlin.de>
2015-12-08 21:51:18 +0100

    Added email templates for the manger and the employee. can be used in the send mail method.

--- backend/src/main/java/de/tuberlin/enterprisecomputing/integrations/MailService.java
+++ backend/src/main/java/de/tuberlin/enterprisecomputing/integrations/MailService.java
@@ -14,4 +14,30 @@ public class MailService {
     public void sendMail(final String to, final String content){
         //TODO send email
     }
+    
+    // Returns Email template for the manager
+    private String[] managerEmail() {
+    	String subject = "New reimbursement submission!";
+    	String body = String
+				.format("This is an automatically generated E-mail! Do not respond!"
+						+ "\n\nAn employee has made a new submission for a reimbursement!"
+						+ "\nYou can accept/decline the reimbursement at:"
+						+ "\nhttp://reimbursement-frontend.mybluemix.net/manager"
+						);
+    	String[] mail = {subject, body};
+    	return mail;
+    }
+    
+    // Returns Email template for the employee
+    private String[] employeeEmail() {
+    	String subject = "Answer to reimbursement submission!";
+    	String body = String
+				.format("This is an automatically generated E-mail! Do not respond!"
+						+ "\n\nThe manager has answered to your reimbursement submission!"
+						+ "\nYou can see the status to your reimbursement at:"
+						+ "\nhttp://reimbursement-frontend.mybluemix.net/employee"
+						);
+    	String[] mail = {subject, body};
+    	return mail;
+    }
 }
