5283fb226f726de2da6a4437135dd874b2bf9f7d
The-Anh <theanhly@mailbox.tu-berlin.de>
2015-12-09 19:39:27 +0100

    Update employee example to display right date format

--- frontend/app.js
+++ frontend/app.js
@@ -20,13 +20,13 @@ app.set('view engine', 'jade');
 var appEnv = cfenv.getAppEnv();
 //test array with all the reimbursements
 var reimbursements = [
-	                    {id:'1', name:'Hans', where:'Berlin', why:'Party', when:'11.05.2014', amount:'1337', document:'expenses.pdf', status: 'accepted'},
-	                    {id:'2', name:'Thomas', where:'Berlin', why:'Party', when:'13.05.2014', amount:'134', document:'expenses.xlsx', status: 'unknown'},
-	                    {id:'3', name:'Klaus', where:'Berlin', why:'Party', when:'13.05.2014', amount:'134', document:'expenses.xlsx', status: 'unknown'},
-	                    {id:'4', name:'Hannes', where:'Berlin', why:'Party', when:'13.05.2014', amount:'134', document:'expenses.xlsx', status: 'declined'},
-	          			{id:'5', name:'Hans', where:'Berlin', why:'Party', when:'12.05.2014', amount:'160', document:'expenses.docx', status: 'accepted'},
-	          		    {id:'6', name:'Claudia', where:'Bremen', why:'Party', when:'12.05.2014', amount:'160', document:'expenses.docx', status: 'declined'},
-	        			{id:'7', name:'Hans', where:'Berlin', why:'Party', when:'13.05.2014', amount:'134', document:'expenses.xlsx', status: 'unknown'}
+	                    {id:'1', name:'Hans', where:'Berlin', why:'Party', when:'2014-05-15', amount:'1337', document:'expenses.pdf', status: 'accepted'},
+	                    {id:'2', name:'Thomas', where:'Berlin', why:'Party', when:'2015-02-22', amount:'13984', document:'expenses.xlsx', status: 'unknown'},
+	                    {id:'3', name:'Klaus', where:'Moskau', why:'Accident', when:'2015-02-15', amount:'1324', document:'expenses.xlsx', status: 'unknown'},
+	                    {id:'4', name:'Hannes', where:'Hamburg', why:'Travel', when:'2014-05-15', amount:'1634', document:'expenses.xlsx', status: 'declined'},
+	          			{id:'5', name:'Hans', where:'Berlin', why:'Party', when:'2012-05-16', amount:'16067', document:'expenses.docx', status: 'accepted'},
+	          		    {id:'6', name:'Claudia', where:'Bremen', why:'Travel', when:'2014-03-15', amount:'420', document:'expenses.docx', status: 'declined'},
+	        			{id:'7', name:'Hans', where:'Berlin', why:'Party', when:'2013-12-15', amount:'4267', document:'expenses.xlsx', status: 'unknown'}
 	          			];
 app.listen(appEnv.port, '0.0.0.0', function () {
     console.log("server starting on " + appEnv.url);
