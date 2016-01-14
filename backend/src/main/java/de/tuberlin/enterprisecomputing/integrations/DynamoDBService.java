76ab7d7d994211c1fedd2f10194370bd94f65a89
rigvedpatki <rigved.patki@gmail.com>
2016-01-14 01:13:55 +0100

    Modified updateRequest() and setStatus()

--- backend/src/main/java/de/tuberlin/enterprisecomputing/integrations/DynamoDBService.java
+++ backend/src/main/java/de/tuberlin/enterprisecomputing/integrations/DynamoDBService.java
@@ -7,26 +7,24 @@ import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-import com.amazonaws.services.dynamodbv2.document.AttributeUpdate;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
 
 import com.amazonaws.services.dynamodbv2.document.UpdateItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
 import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ReturnValue;
 import com.amazonaws.services.dynamodbv2.model.ScanRequest;
 import com.amazonaws.services.dynamodbv2.model.ScanResult;
 
 import de.tuberlin.enterprisecomputing.domain.EmployeeRequest;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 @Service
 public class DynamoDBService {
@@ -49,27 +47,11 @@ public class DynamoDBService {
         dynamoDB = new DynamoDB(dynamoDBClient);
     }
 
-    public String createRequestEntry(final EmployeeRequest request, final boolean fileAttached) {
+    public String createRequestEntry(final EmployeeRequest request) {
 
         //Selecting the table from DynamoDB
         Table employeeRequestTable = dynamoDB.getTable(tableName);
         System.out.println("Table create request entry: " + employeeRequestTable.toString());
-        //Generating Random UUID as requestId
-        String uniqueRequestId = UUID.randomUUID().toString();
-        request.setRequestId(uniqueRequestId);
-        //Generating the current timestamp
-        Long currentTimeStamp = new Date().getTime();
-        request.setTimestamp(currentTimeStamp.toString());
-        //generating a URL to the document
-        S3Service s3 = new S3Service();
-        if (fileAttached) {
-            String docLink = s3.generateURL(request.getRequestId());
-            request.setDocument(docLink);
-        } else {
-            request.setDocument("No document attached");
-        }
-        //Setting the status of document
-        request.setStatus("Unchecked");
         try {
             //Building the requestEntryItem
             Item requestEntryItem = new Item()
@@ -80,7 +62,8 @@ public class DynamoDBService {
                     .withString("why", request.getWhy())
                     .withString("where", request.getWhere())
                     .withInt("amount", request.getAmount())
-                    .withString("document", request.getDocument())
+                    .withString("documentLink", request.getDocumentLink())
+                    .withString("documentName", request.getDocumentName())
                     .withString("status", request.getStatus());
 
 
@@ -125,7 +108,8 @@ public class DynamoDBService {
                 temp.setWhy(item.get("why").getS());
                 temp.setWhere(item.get("where").getS());
                 temp.setAmount(Integer.parseInt(item.get("amount").getN()));
-                temp.setDocument(item.get("document").getS());
+                temp.setDocumentLink(item.get("documentLink").getS());
+                temp.setDocumentName(item.get("documentName").getS());
                 temp.setStatus(item.get("status").getS());
                 //System.out.println(temp);
                 allRequests.add(temp);
@@ -142,11 +126,11 @@ public class DynamoDBService {
 
         //Selecting the table from DynamoDB
         Table employeeRequestTable = dynamoDB.getTable(tableName);
-        System.out.println("Dynamodb table: " + dynamoDB.getTable("employee_reimbursements").toString());
-        System.out.println("Table get request by id: " + employeeRequestTable.toString());
+        //System.out.println("Dynamodb table: " + dynamoDB.getTable("employee_reimbursements").toString());
+        //System.out.println("Table get request by id: " + employeeRequestTable.toString());
         // Getting the requestEntry for requestId = id
         Item requestEntryItem = employeeRequestTable.getItem("requestId", id);
-        System.out.println(requestEntryItem.toString());
+        //System.out.println(requestEntryItem.toString());
         // Creating an EmployeeRequest instance to be returned
         EmployeeRequest request = new EmployeeRequest();
         //Filling up the instance with data
@@ -157,7 +141,8 @@ public class DynamoDBService {
         request.setWhere(requestEntryItem.getString("where"));
         request.setWhy(requestEntryItem.getString("why"));
         request.setAmount(requestEntryItem.getInt("amount"));
-        request.setDocument(requestEntryItem.getString("document"));
+        request.setDocumentLink(requestEntryItem.getString("documentLink"));
+        request.setDocumentName(requestEntryItem.getString("documentName"));
         request.setStatus(requestEntryItem.getString("status"));
 
 
@@ -167,22 +152,28 @@ public class DynamoDBService {
     public void updateRequest(final String id, final EmployeeRequest request) {
 
         Table employeeRequestTable = dynamoDB.getTable(tableName);
-        System.out.println("Table updateRequest: " + employeeRequestTable.toString());
+        //System.out.println("Table updateRequest: " + employeeRequestTable.toString());
         //Creating UpdateItemSpec for the updates to be made
+        
         UpdateItemSpec requestEntryUpdate = new UpdateItemSpec()
-                .withPrimaryKey("requestID", id)
+                .withPrimaryKey("requestId", id)
+                .withUpdateExpression("set #time=:val1, #when=:val2, #why=:val3, #where=:val4, #amount=:val5")
+                .withNameMap(new NameMap()
+                		.with("#time","timestamp")
+                		.with("#when","when")
+                		.with("#why","why")
+                		.with("#where","where")
+                		.with("#amount","amount"))
                 .withValueMap(new ValueMap()
-                        .withString("name", request.getName())
-                        .withString("timestamp", request.getTimestamp())
-                        .withString("when", request.getWhen())
-                        .withString("why", request.getWhy())
-                        .withString("where", request.getWhere())
-                        .withInt("amount", request.getAmount())
-                );
-        System.out.println("Updated request: "+ requestEntryUpdate.toString());
+                        .withString(":val1",request.getTimestamp())
+                        .withString(":val2", request.getWhen())
+                        .withString(":val3", request.getWhy())
+                        .withString(":val4", request.getWhere())
+                        .withInt(":val5", request.getAmount()))
+                .withReturnValues(ReturnValue.UPDATED_NEW);
         //Updating the employeeRequestTable
         UpdateItemOutcome employeeRequestOutcome = employeeRequestTable.updateItem(requestEntryUpdate);
-        System.out.println("Outcome: "+ employeeRequestOutcome.toString());
+        System.out.println("Outcome: "+ employeeRequestOutcome.getItem().toJSONPretty());
         //employeeRequestTable.updateItem(requestEntryUpdate);
     }
 
@@ -191,18 +182,28 @@ public class DynamoDBService {
         //Selecting the table from DynamoDB
         Table employeeRequestTable = dynamoDB.getTable(tableName);
         System.out.println("Table setStatus: " + employeeRequestTable.toString());
+        
+        //Creating UpdateItemSpec for the updates to be made
+        
+        UpdateItemSpec requestEntryUpdate = new UpdateItemSpec()
+                .withPrimaryKey("requestId", id)
+                .withUpdateExpression("set #s=:val1")
+                .withNameMap(new NameMap()
+                		.with("#s", "status"))
+                .withValueMap(new ValueMap()
+                        .withString(":val1", status))
+                .withReturnValues(ReturnValue.UPDATED_NEW);
         //Updating the requestStatus as status for requestId = id
-        //UpdateItemOutcome employeeRequestOutcome = employeeRequestTable.updateItem( id, new AttributeUpdate("status").put(status));
-        employeeRequestTable.updateItem(id, new AttributeUpdate("status").put(status));
-
+        UpdateItemOutcome employeeRequestOutcome = employeeRequestTable.updateItem(requestEntryUpdate);
+        System.out.println("Outcome: "+ employeeRequestOutcome.getItem().toJSONPretty());
     }
 
-    public void setDocLink(final String id, final String docLink) {
+/*    public void setDocLink(final String id, final String docLink) {
         //Selecting the table from DynamoDB
         Table employeeRequestTable = dynamoDB.getTable(tableName);
         System.out.println("Table set doclink: " + employeeRequestTable.toString());
         //Updating the requestStatus as status for requestId = id
-        /*UpdateItemOutcome employeeRequestOutcome =*/
-        employeeRequestTable.updateItem(id, new AttributeUpdate("document").put(docLink));
-    }
+        UpdateItemOutcome employeeRequestOutcome =
+        employeeRequestTable.updateItem(id, new AttributeUpdate("documentLink").put(docLink));
+    }*/
 }
